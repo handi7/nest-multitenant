759e1fd16959f3ec82dc621d2ac7c9bc5cb93752
handi <handi7.co@gmail.com>
2025-06-20 23:00:44 +0700

    update branch service

--- src/modules/branch/branch.controller.ts
+++ src/modules/branch/branch.controller.ts
@@ -1,33 +1,39 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from "@nestjs/common";
 import { BranchService } from "./branch.service";
 import { CreateBranchDto, UpdateBranchDto } from "./branch.dto";
+import { Permissions } from "src/common/decorators/permissions.decorator";
+import { PermissionEnum } from "prisma/client";
 
 @Controller()
 export class BranchController {
   constructor(private readonly branchService: BranchService) {}
 
   @Post("branch")
+  @Permissions(PermissionEnum.branch_create)
   create(@Req() req: AppRequest, @Body() createBranchDto: CreateBranchDto) {
     return this.branchService.create(req.user, createBranchDto);
   }
 
   @Get("branches")
+  @Permissions(PermissionEnum.branch_view)
   findAll(@Req() req: AppRequest) {
     return this.branchService.findAll(req.user);
   }
 
   @Get("branch/:id")
-  findOne(@Param("id") id: string) {
-    return this.branchService.findOne(+id);
+  @Permissions(PermissionEnum.branch_view)
+  findOne(@Param("id") id: string, @Req() req: AppRequest) {
+    return this.branchService.findOne(id, req.user);
   }
 
   @Patch("branch/:id")
-  update(@Param("id") id: string, @Body() updateBranchDto: UpdateBranchDto) {
-    return this.branchService.update(+id, updateBranchDto);
+  @Permissions(PermissionEnum.branch_edit)
+  update(@Param("id") id: string, @Body() dto: UpdateBranchDto, @Req() req: AppRequest) {
+    return this.branchService.update(id, dto, req.user);
   }
 
   @Delete("branch/:id")
-  remove(@Param("id") id: string) {
-    return this.branchService.remove(+id);
+  remove(@Param("id") id: string, @Req() req: AppRequest) {
+    return this.branchService.remove(id, req.user);
   }
 }
--- src/modules/branch/branch.service.ts
+++ src/modules/branch/branch.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateBranchDto, UpdateBranchDto } from "./branch.dto";
 import { Prisma } from "prisma/client";
@@ -43,15 +48,80 @@ export class BranchService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} branch`;
+  async findOne(id: string, user: UserType) {
+    try {
+      const branch = await this.prisma.branch.findUnique({
+        where: { id, tenant_id: user.tenant_id },
+      });
+
+      if (!branch) {
+        throw new NotFoundException("Branch not found.");
+      }
+
+      return branch;
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
-  update(id: number, updateBranchDto: UpdateBranchDto) {
-    return `This action updates a #${id} branch`;
+  async update(id: string, dto: UpdateBranchDto, user: UserType) {
+    try {
+      const branch = await this.prisma.branch.findUnique({
+        where: { id, tenant_id: user.tenant_id },
+      });
+
+      if (!branch) {
+        throw new NotFoundException("Branch not found.");
+      }
+
+      const existingBranch = await this.prisma.branch.findFirst({
+        where: {
+          tenant_id: user.tenant_id,
+          name: { equals: dto.name, mode: "insensitive" },
+          id: { not: id }, // Exclude current branch
+        },
+      });
+
+      if (existingBranch) {
+        throw new ConflictException({
+          validation: { name: "Branch already exists." },
+        });
+      }
+
+      return await this.prisma.branch.update({
+        where: { id },
+        data: { name: dto.name },
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} branch`;
+  async remove(id: string, user: UserType) {
+    try {
+      const branch = await this.prisma.branch.findUnique({
+        where: { id, tenant_id: user.tenant_id },
+      });
+
+      if (!branch) {
+        throw new NotFoundException("Branch not found.");
+      }
+
+      // Check if branch is used in any role
+      const roleCount = await this.prisma.userRole.count({
+        where: { branch_id: id },
+      });
+
+      if (roleCount > 0) {
+        throw new BadRequestException("Branch cannot be deleted because it is used in roles.");
+      }
+
+      return await this.prisma.branch.update({
+        where: { id },
+        data: { deleted_at: new Date() },
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 }
